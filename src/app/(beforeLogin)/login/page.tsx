32b851cc49f783d4cd558b4dede68603f600c353
Big One <sponjjanc@naver.com>
2023-12-23 00:00:20 +0900

    :bug: fix: routing 관련 문제

--- src/app/(beforeLogin)/login/page.tsx
+++ src/app/(beforeLogin)/login/page.tsx
@@ -1,9 +1,14 @@
 "use client";
+
 import { useRouter } from "next/navigation";
 import MainPage from "@/app/(beforeLogin)/_component/main";
+import { useEffect } from "react";
 
 export default function RedirectLoginPage() {
   const router = useRouter();
-  router.replace("/i/flow/login");
+  useEffect(() => {
+    router.replace("/i/flow/login");
+  }, []);
+
   return <MainPage />;
 }

ab3184ea0129fc8e2e10599dcb8a92e15de6d7d6
Big One <sponjjanc@naver.com>
2023-12-28 20:54:45 +0900

    :sparkles: feat: 특정 경로에서는 사라지게 하기위해 pathname 에 따라 분기처리

--- src/app/(afterLogin)/_component/trend-section.tsx
+++ src/app/(afterLogin)/_component/trend-section.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import style from "./trend-section.module.css";
 import Trend from "@/app/(afterLogin)/_component/trend";
+import { usePathname } from "next/navigation";
 export default function TrendSection() {
+  const pathname = usePathname();
+
+  if (pathname === "/explore") return null;
+
   return (
     <div className={style.trendBackground}>
       <div className={style.trendContainer}>

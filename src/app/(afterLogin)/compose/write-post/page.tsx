ae14694b3981cab202fc1666b5546039a737beb2
Big One <sponjjanc@naver.com>
2023-12-28 15:28:57 +0900

    :sparkles: write-post page

---
+++ src/app/(afterLogin)/compose/write-post/page.tsx
@@ -0,0 +1,11 @@
+import HomePage from "@/app/(afterLogin)/home/page";
+import PostModal from "@/app/(afterLogin)/@modal/(.)compose/write-post/page";
+
+export default function PostPage() {
+  return (
+    <>
+      <HomePage />
+      <PostModal />
+    </>
+  );
+}

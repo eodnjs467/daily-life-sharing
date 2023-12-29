b9652eff965dac3ab848585bdb2c8d1997cda12c
Big One <sponjjanc@naver.com>
2023-12-29 23:58:53 +0900

    :lipstick: ui: 컴포넌트를 주소에 따라 렌더링 제한을 하기위해 usePathname 을 사용하여 분기처리

--- src/app/(afterLogin)/_component/right-search-zone.tsx
+++ src/app/(afterLogin)/_component/right-search-zone.tsx
@@ -1,6 +1,13 @@
+"use client";
+
 import style from "./right-search-zone.module.css";
 import SearchForm from "@/app/(afterLogin)/_component/search-form";
+import { usePathname } from "next/navigation";
 export default function RightSearchZone() {
+  const pathname = usePathname();
+  if (pathname === "/explore") return null;
+  if (pathname === "/search") {
+  }
   return (
     <div className={style.container}>
       <SearchForm />
